4a9ee113b905bc50f90d0e1418539523dffba723
George Reznik <nereznik@gmail.com>
2022-11-26 18:33:09 +0500

    easy slot game

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import * as PIXI from 'pixi.js';
+import { config } from './config';
+import PlayButton from './components/PlayButton';
+import Reels from './components/Reels';
+
+import './style.css';
+
+const { gameWidth, gameHeight } = config;
+
+function createApplication(): PIXI.Application {
+  const app = new PIXI.Application({
+    backgroundColor: 0xd3d3d3,
+    width: gameWidth,
+    height: gameHeight
+  });
+  app.renderer.resize(window.innerWidth, window.innerHeight);
+  app.stage.scale.x = window.innerWidth / gameWidth;
+  app.stage.scale.y = window.innerHeight / gameHeight;
+  window.addEventListener('resize', () => {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    app.stage.scale.x = window.innerWidth / gameWidth;
+    app.stage.scale.y = window.innerHeight / gameHeight;
+  });
+  return app;
+}
+
+function loadAssets(onComplete: () => void): void {
+  const loader = PIXI.Loader.shared;
+  loader.onComplete.once(onComplete);
+  loader.load();
+}
+
+function render(app: PIXI.Application) {
+  document.body.appendChild(app.view);
+}
+
+window.onload = () =>
+  loadAssets(() => {
+    const app = createApplication();
+    const stage = app.stage;
+
+    const button = new PlayButton(config);
+    stage.addChild(button);
+
+    const reels = new Reels(config, app.ticker);
+    stage.addChild(reels);
+
+    button.on('click', function (this: PlayButton) {
+      if (!reels.areSpinning()) {
+        this.setDisabled();
+        reels.spin(() => {
+          this.setInactive();
+        });
+      }
+    });
+
+    render(app);
+  });
